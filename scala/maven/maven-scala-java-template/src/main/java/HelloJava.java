522578e49289d36102cb92614e2bc7bb7263512c
Abilash  Thomas <984620@India.TCS.com>
2016-08-29 18:24:59 +0530

    maven scala another hello world

---
+++ scala/maven/maven-scala-java-template/src/main/java/HelloJava.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 984620 on 8/3/2016.
+ */
+public class HelloJava {
+    public static void main(String args[]){
+        System.out.println("Hello java in scala");
+    }
+}
---
+++ scala/maven/maven-scala-java-template/src/main/scala/HelloScala.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by 984620 on 8/3/2016.
+  */
+object HelloScala {
+  /* This is my first java program.
+ * This will print 'Hello World' as the output
+ */
+  def main(args: Array[String]) {
+    println("Hello, scala!") // prints Hello World
+  }
+}
