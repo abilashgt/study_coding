63c42c9ecca893d44a78f991a4ce0c35d512fb95
abilash <abilashgeorgethomas@gmail.com>
2016-10-30 16:41:29 +0530

    initialise scala basic project all basic study

---
+++ scala/basic_project/src/main/scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by abilash on 3/9/16.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit ={
+    print("Hello World")
+  }
+}
---
+++ scala/basic_project/src/main/scala/collections/ListExample.scala
@@ -0,0 +1,15 @@
+package collections
+
+/**
+  * Created by abilash on 30/10/16.
+  */
+object ListExample {
+  def main(args: Array[String]): Unit ={
+    val testInt = List(0, 1, 2)
+    val testString = List("aa","bb","cc")
+
+    println(testInt)
+    println(testString)
+  }
+
+}
